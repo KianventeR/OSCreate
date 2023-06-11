24ee5a9e710a3c4784a88801a756a2e8cce27d84
KianventeR <gabiana06@gmail.com>
2023-06-11 13:43:38 +0800

    added dependency

--- src/QuestionDirectory.java
+++ src/QuestionDirectory.java
@@ -1,3 +1,32 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 public class QuestionDirectory {
-    
+    XSSFWorkbook wb;
+    XSSFSheet sheet;
+    List<Integer> generated;
+
+    public void initiateWB() {
+        try {
+            wb = new XSSFWorkbook(getClass().getClassLoader().getResourceAsStream("questions.xlsx"));
+            sheet = wb.getSheetAt(0);
+        } catch (IOException e) {
+        }
+    }
+
+    public void test() {
+        generated = new LinkedList<>();
+        CellReference cellReferenceQues = new CellReference("E" + generated.get(2).toString()); 
+        Row rowQues = sheet.getRow(cellReferenceQues.getRow());
+        Cell cellQues = rowQues.getCell(cellReferenceQues.getCol());
+    }
+
 }
